1a1529b7883606a21881ec4bc08184645ebc62c2
Zzz310304 <trucnguyen201173@gmail.com>
2023-04-23 21:45:36 +0700

    file sprite

--- src/uet/oop/bomberman/entities/Grass.java
+++ src/uet/oop/bomberman/entities/Grass.java
@@ -3,7 +3,7 @@ package uet.oop.bomberman.entities;
 import javafx.scene.image.Image;
 
 public class Grass extends Entity {
-
+//cỏ cây
     public Grass(int x, int y, Image img) {
         super(x, y, img);
     }
