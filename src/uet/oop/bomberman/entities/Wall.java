dc99a3744977202719c170078db0cb848a3923bc
Zzz310304 <trucnguyen201173@gmail.com>
2023-04-23 21:46:49 +0700

    Update Wall.java

--- src/uet/oop/bomberman/entities/Wall.java
+++ src/uet/oop/bomberman/entities/Wall.java
@@ -3,7 +3,7 @@ package uet.oop.bomberman.entities;
 import javafx.scene.image.Image;
 
 public class Wall extends Entity {
-
+// tường
     public Wall(int x, int y, Image img) {
         super(x, y, img);
     }
