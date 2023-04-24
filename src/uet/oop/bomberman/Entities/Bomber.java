695e1abc04107b1983daac5f44b1c30cdee147d2
Zzz310304 <trucnguyen201173@gmail.com>
2023-04-24 08:47:17 +0700

    thay doi class Entities

--- src/uet/oop/bomberman/Entities/Bomber.java
+++ src/uet/oop/bomberman/Entities/Bomber.java
@@ -1,10 +1,6 @@
 package uet.oop.bomberman.Entities;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 
 public class Bomber extends Entity {
 
