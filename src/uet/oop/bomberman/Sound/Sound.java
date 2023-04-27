59837ca7cd0c8dd24a9a7b68594308737858cc68
vankhai <vankhai26022004@gmail.com>
2023-04-27 14:22:36 +0700

    push_sound

---
+++ src/uet/oop/bomberman/Sound/Sound.java
@@ -0,0 +1,42 @@
+package uet.oop.bomberman.Sound;
+
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.util.Objects;
+
+public class Sound {
+
+    private static Clip myClip;
+
+    public static void play(String sound) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    myClip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            Objects.requireNonNull(Main.class.getResourceAsStream("/sound/" + sound + ".wav")));
+                    myClip.open(inputStream);
+                    myClip.start();
+                    if (sound.equals("Back")) {
+                        loopInf();
+                    } else {
+                        return;
+                    }
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+
+    }
+
+    public static void loopInf(){
+        if (myClip.isActive()){
+            return;
+        }
+        myClip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+}
